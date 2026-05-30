a43211ad5b82191b679eb4b98d80ce6164b2eb72
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-05-30 23:17:26 +0200

    Controller.

---
+++ FormatTEST/FormatTEST/Controllers/CoursesController.cs
@@ -0,0 +1,75 @@
+using FormatTEST.Data;
+using FormatTEST.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormatTEST.Controllers;
+
+public class CoursesController : ControllerBase
+{
+    private readonly UniversityTasksDbContext _context;
+
+    public CoursesController(UniversityTasksDbContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> getCourses([FromQuery] bool activeOnly = true)
+    {
+        var query = _context.Courses.AsNoTracking();
+
+
+        if (activeOnly)
+        {
+            query = query.Where(c =>  c.IsActive);
+        }
+
+
+        var courses = await query.Select(c => new CourseDto()
+        {
+            CourseId = c.CourseId,
+            Code = c.Code,
+            Name = c.Name,
+            Credits = c.Credits,
+            AssignmentCount =  c.Assignments.Count
+        }).ToListAsync();
+        return Ok(courses);
+        
+        
+    }
+
+    [HttpGet("{idCourse}/assignments")]
+
+    public async Task<IActionResult> GetCourseAssignments(int idCourse, [FromQuery] bool publishedOnly = true)
+    {
+
+
+        var courseExists = await _context.Courses.AnyAsync(c => c.CourseId == idCourse);
+
+        if (!courseExists)
+        {
+            return NotFound("Course not found");
+        }
+        
+        var query = _context.Assignments.AsNoTracking().Where(a => a.CourseId == idCourse);
+
+        if (publishedOnly)
+        {
+            query = query.Where(a => a.IsPublished);
+        }
+        
+        var assigments = await query.Select(a => new AssignmentDto()
+        {
+            AssignmentId =  a.AssignmentId,
+            Title =  a.Title,
+            DueDate =  a.DueDate,
+            MaxPoints =  a.MaxPoints,
+            IsPublished =  a.IsPublished,
+            SubmissionCount =  a.Submissions.Count
+        }).ToListAsync();
+        return Ok(assigments);
+    }
+    
+}
